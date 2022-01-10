5c87e59ec2532a1d299824a051c54e93bec6b415
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-01-10 23:15:52 +1100

    added geocode cities API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,40 @@
-function getCity() {
-    fetch("https://nominatim.openstreetmap.org/search?city=sydney&format=geocodejson");
-}
+var cityEl = document.querySelector("#city");
+var btnEl = document.querySelector(".btn");
+
+// var formSubmitHandler = function (event) {
+//     event.preventDefault();
+
+//     var city = cityEl.value.trim();
+
+//     if (city) {
+//         getCity(city);
+//     } else {
+//         alert("Please enter a city");
+//     };
+// };
+
+var getCity = function (city) {
+    var apiUrl = "https://nominatim.openstreetmap.org/search?city=" + city + "&format=geocodejson";
+
+    fetch(apiUrl)
+      .then(function (response){
+          if (response.ok) {
+              console.log(response);
+              response.json().then(function (data){
+                  console.log(data);
+              })
+          }
+      });
+};
+
+getCity("sydney");
+
+
 
 function getApi() {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
 
     
 }
+
+// btnEl.addEventListener("submit", formSubmitHandler);
